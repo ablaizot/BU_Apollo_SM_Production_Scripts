3811d006a20c92519448e5cf116dd4d35ca8b4f9
ablaizot <ablaizot@bu.edu>
2025-07-21 17:49:30 -0400

    fix dth tcl

--- IBERTpy/dth_eyescan.tcl
+++ IBERTpy/dth_eyescan.tcl
@@ -1,4 +1,6 @@
+
 open_hw_manager
+connect_hw_server -allow_non_jtag
 
 current_hw_target [get_hw_targets */xilinx_tcf/Xilinx/00001ee7fd5e01]
 set_property PARAM.FREQUENCY 6000000 [get_hw_targets */xilinx_tcf/Xilinx/00001ee7fd5e01]
