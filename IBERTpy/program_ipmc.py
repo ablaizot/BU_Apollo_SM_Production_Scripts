cea091acda09673fcbffdbbb0f23347f04ae6fba
ablaizot <ablaizot@bu.edu>
2025-07-22 14:57:15 -0400

    find mac cmd

--- IBERTpy/program_ipmc.py
+++ IBERTpy/program_ipmc.py
@@ -13,12 +13,19 @@ def get_mac_address(serial_num):
     """
     try:
         mac_cmd = []
+
+        look = [f"apollo{serial_num}-0", f"apollo{serial_num}-1" , f"ipmc{serial_num}"]
+        
         with open('mac_addr.csv', 'r') as f:
-            reader = csv.reader(f, delimiter=' ')
+            reader = csv.reader(f, delimiter='\t')
             for row in reader:
                 print(f"Processing row: {row}")  # Debugging output
-                if len(row) >= 4 and row[0] == serial_num and row[1] == 'eth0':
-                    mac_cmd.append(row[9])
+                if len(row) >= 4:
+                    for key in look:
+                        if key in row[4]:
+                            print(f"Found matching row for {key}: {row}")
+                            mac_cmd.append(row[9])
+                            print(f"Added MAC command: {row[9]}")
                 
                 
         return mac_cmd
@@ -34,11 +41,11 @@ def send_command_to_ipmc(serial_number, port='/dev/ttyACM1', baudrate=115200):
         ser.write(b'eepromrd\n')
         s = ser.read(100)
         print(s.decode('utf-8'))
-
+        idwr = f'idwr {serial_number}\n'.format(serial_number=serial_number)
         ser.write(b'verwr 2\n')
         ser.write(b'revwr 3\n')
         ser.write(b'bootmode 3\n')
-        ser.write(b'idwr {f"{serial_number}"}\n'.format(serial_number=serial_number))
+        ser.write(idwr.encode('utf-8'))
 
         for mac_addr in mac_cmds:
             ser.write(mac_addr.encode('utf-8') + b'\n')
