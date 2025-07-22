ca35853e39b894bf7b291283dd6761077fbb5e2f
ablaizot <ablaizot@bu.edu>
2025-07-22 14:05:24 -0400

    return and read

--- IBERTpy/program_ipmc.py
+++ IBERTpy/program_ipmc.py
@@ -3,12 +3,16 @@ import serial
 def send_command_to_ipmc(serial_number, port='/dev/ttyACM1', baudrate=115200):
 
     with serial.Serial(port, baudrate, timeout=1) as ser:
-        ser.write(b'eepromrd')
+        ser.write(b'eepromrd\n')
         s = ser.read(100)
         print(s.decode('utf-8'))
-        ser.write(b'verwr 2')
-        ser.write(b'revwr 3')
-        ser.write(b'bootmode 3')
+
+        ser.write(b'verwr 2\n')
+        ser.write(b'revwr 3\n')
+        ser.write(b'bootmode 3\n')
+        ser.write(b'bootmode 2\n')
+
+        s = ser.read(200)
         ser.close()
 
 
