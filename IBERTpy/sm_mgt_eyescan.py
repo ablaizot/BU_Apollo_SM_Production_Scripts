4bbe3746b5bac74268052386575349e15742038d
ablaizot <ablaizot@bu.edu>
2025-07-25 09:34:21 -0400

    fix

--- IBERTpy/sm_mgt_eyescan.py
+++ IBERTpy/sm_mgt_eyescan.py
@@ -261,6 +261,7 @@ if __name__ == "__main__":
                     user='root',
                     connect_kwargs={"password": password}
                 ) as conn:
+                conn.run('echo "Connection successful"')
                 conn_est = True
                 print(f"Device {hostname} is reachable.")
         except (socket.error, ConnectionError) as e:
