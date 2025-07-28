20a8a481fa6fb8096554a8e1d17fb80bd6393236
ablaizot <ablaizot@bu.edu>
2025-07-28 14:39:40 -0400

    ipmc shell

--- IBERTpy/program_ipmc.sh
+++ IBERTpy/program_ipmc.sh
@@ -1,8 +1,10 @@
-stty -F /dev/ttyUSB0 115200 raw -echo   #CONFIGURE SERIAL PORT
-exec 3</dev/ttyUSB0                     #REDIRECT SERIAL OUTPUT TO FD 3
+#!/usr/bin/env bash
+
+stty -F /dev/ttyACM1 115200 raw -echo   #CONFIGURE SERIAL PORT
+exec 3</dev/ttyACM1                     #REDIRECT SERIAL OUTPUT TO FD 3
   cat <&3 > /tmp/ttyDump.dat &          #REDIRECT SERIAL OUTPUT TO FILE
   PID=$!                                #SAVE PID TO KILL CAT
-    echo "R" > /dev/ttyUSB0             #SEND COMMAND STRING TO SERIAL PORT
+    echo "eepromrd" > /dev/ttyACM1           #SEND COMMAND STRING TO SERIAL PORT
     sleep 0.2s                          #WAIT FOR RESPONSE
   kill $PID                             #KILL CAT PROCESS
   wait $PID 2>/dev/null                 #SUPRESS "Terminated" output
